d485d8f98d91095a721bbeee72c1917e6a259e5e
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-17 21:16:52 +0700

    Modifikasi File Index.js dan app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import React, { useState, Fragment } from "react";
-import axios from "axios";
-
+import React from "react";
+import "./app.css";
+import InputShortener from "./Component/InputShortener";
 function App() {
   return (
-    <Fragment>
-      <div className="container text-center">
-        <h1>URL Shortener</h1>
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Masukkan URL Anda"
-            aria-describedby="basic-addon2"
-          />
-          <div className="input-group-append">
-            <span className="input-group-text" id="basic-addon2">
-              @example.com
-            </span>
-          </div>
-        </div>
-      </div>
-    </Fragment>
+    <div className="container">
+      <InputShortener />
+    </div>
   );
 }
 
