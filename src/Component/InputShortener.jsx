5379dc94d9965d791f4962b39585ec774aa5edc6
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-17 21:17:28 +0700

    Menambahkan File BackgroundAnimated.jsx dan inputSHorten.jsx

---
+++ src/Component/InputShortener.jsx
@@ -0,0 +1,17 @@
+import React, { Fragment } from "react";
+import "../app.css";
+export default function InputShortener() {
+  return (
+    <Fragment>
+      <div className="inputContainer">
+        <h1>
+          URL <span>Shortener</span>
+          <div>
+            <input type="text" placeholder="Paste URL Kamu disini" />
+            <button type="button">Shorten</button>
+          </div>
+        </h1>
+      </div>
+    </Fragment>
+  );
+}
