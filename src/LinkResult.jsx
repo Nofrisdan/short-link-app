166a04e81caeab0288d2860746b9a1f843982eca
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-18 13:29:52 +0700

    Install react-copy-to-clipboard

--- src/LinkResult.jsx
+++ src/LinkResult.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 
 function LinkResult() {
-  return <h1>Link Result</h1>;
+  const [result, setResult] = useState("https://url.cakdunsite/hyagsh");
+
+  return (
+    <div className="result">
+      <p>{result}</p>
+      <button>Copy To Clipboard</button>
+    </div>
+  );
 }
 
 export default LinkResult;
