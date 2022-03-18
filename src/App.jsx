eadfb4cf25703622ef659d1f537a4dd7f3a514f9
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-18 13:01:37 +0700

    Menambahkan Component Link Result

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./app.css";
 import InputShortener from "./Component/InputShortener";
 import BackgroundAnimate from "./Component/BackgroundAnimate";
 import Copyright from "./Component/Copyright";
+import LinkResult from "./LinkResult";
 function App() {
   return (
     <div className="container">
       <InputShortener />
       <BackgroundAnimate />
       <Copyright />
+      <LinkResult />
     </div>
   );
 }
---
+++ src/LinkResult.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+function LinkResult() {
+  return <h1>Link Result</h1>;
+}
+
+export default LinkResult;
