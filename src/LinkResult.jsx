d61272134e82b6a6cf5601c54ec93c245a2ae523
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-19 15:40:13 +0700

    Modifikasi Link result dan app.css

--- src/LinkResult.jsx
+++ src/LinkResult.jsx
@@ -3,18 +3,14 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import Swal from "sweetalert2";
 function LinkResult() {
   const [result, setResult] = useState("https://url.cakdunsite/hyagsh");
-  const [count, setCount] = useState("false");
-  useEffect(() => {
-    if (count == "true") {
-      Swal.fire("copy", "", "success");
-    }
-  });
+  const [copy, setCopy] = useState(false);
+
   return (
     <div className="result">
       <p>{result}</p>
 
-      <CopyToClipboard text={result} onCopy={() => setCount("true")}>
-        <button>Copy To Clipboard</button>
+      <CopyToClipboard text={result} onCopy={() => setCopy(true)}>
+        <button className={copy ? "copy" : ""}>Copy To Clipboard</button>
       </CopyToClipboard>
     </div>
   );
