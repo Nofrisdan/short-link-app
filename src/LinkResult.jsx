a032994f4bc43a857ba85dea93761f9fc5a74abe
Nofrisdan <nofrisdansitopu22@gmail.com>
2022-03-19 12:13:09 +0700

    Modifikasi Link Result dan Background

--- src/LinkResult.jsx
+++ src/LinkResult.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import Swal from "sweetalert2";
 function LinkResult() {
   const [result, setResult] = useState("https://url.cakdunsite/hyagsh");
-
+  const [count, setCount] = useState("false");
+  useEffect(() => {
+    if (count == "true") {
+      Swal.fire("copy", "", "success");
+    }
+  });
   return (
     <div className="result">
       <p>{result}</p>
-      <button>Copy To Clipboard</button>
+
+      <CopyToClipboard text={result} onCopy={() => setCount("true")}>
+        <button>Copy To Clipboard</button>
+      </CopyToClipboard>
     </div>
   );
 }
